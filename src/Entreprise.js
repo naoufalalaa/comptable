50e1b941ffe3a0bbf0be73db8f38faa04a0fc4f0
Naoufal Alaa <alaanaoufal2000@gmail.com>
2021-08-07 10:05:44 +0100

    commit

--- src/Entreprise.js
+++ src/Entreprise.js
@@ -21,17 +21,41 @@ export class Profile extends Component{
       changeHandler = e => {
         this.setState({[e.target.name] : e.target.value})
       }
+      changeHandlerA = e => {
+        if(document.getElementById('listAssocies').value===''){
+            document.getElementById('listAssocies').classList.add('uk-form-danger')
+        }else{
+            this.setState({listAssocies : document.getElementById('listAssocies').value})
+            this.setState({nbrAssocies : document.getElementById('nbrAssocies').value})
+            document.getElementById('listA').setAttribute('disabled',true)
+        }
+      }
+      changeHandlerG = e => {
+        if(document.getElementById('liste').value===''){
+            document.getElementById('liste').classList.add('uk-form-danger')
+        }else{
+            this.setState({listGerant : document.getElementById('liste').value})
+            document.getElementById('list').setAttribute('disabled',true)
+        }
+      }
     
+
       handleSubmit = e =>{
         e.preventDefault()
-        
+        this.changeHandlerA();
+        this.changeHandlerG();
+        document.getElementById('submit').setAttribute('disabled',true)
+        document.getElementById('msg').innerHTML=`<div class='uk-alert-success' uk-alert><div uk-spinner></div> LOADING</div>`
         axios.post('https://comptableapi.herokuapp.com/users/ent/create',this.state)
         .then(response => {
             document.getElementById('msg').innerHTML="<div class='uk-alert-success' uk-alert><a class='uk-alert-close' uk-close></a><p>L'entreprise "+response.data.nomE+" a été modifée.</p></div>"
         })
         .catch(err=>{console.log(err)
+            document.getElementById('submit').removeAttribute('disabled')
+            document.getElementById('list').removeAttribute('disabled')
             document.getElementById('msg').innerHTML="<div class='uk-alert-danger' uk-alert><a class='uk-alert-close' uk-close></a><p>Erreur, l'entreprise n'a pas été modifiée.</p></div>"
         })
+        console.log(this.state)
         
       }
 render() {
@@ -39,30 +63,33 @@ render() {
     var list = []
     function add(){
         var ee = document.getElementById('list').value
+        document.getElementById('list').removeAttribute('disabled')
         if(ee){
             list.push(ee)
-            ee = ''
             document.getElementById('liste').value=list
         }
+        document.getElementById('list').value=''
     }
     function min(){
         list.pop()
         document.getElementById('liste').value=list
+        document.getElementById('list').removeAttribute('disabled')
     }
     var listA = []
     function addA(){
         var ee = document.getElementById('listA').value
+        document.getElementById('listA').removeAttribute('disabled')
         if(ee){
             listA.push(ee)
-            ee = ''
-            document.getElementById('listAssoc').value=listA
+            document.getElementById('listAssocies').value=listA
             document.getElementById('nbrAssocies').value=listA.length
         }
         document.getElementById('listA').value=''
     }
     function minA(){
         listA.pop()
-        document.getElementById('listAssoc').value=listA
+        document.getElementById('listAssocies').value=listA
+        document.getElementById('listA').removeAttribute('disabled')
         document.getElementById('nbrAssocies').value=listA.length
     }
     function getCookie(cname) {
@@ -102,7 +129,6 @@ render() {
     }
     
 
-
     
     if (isLogged()) {
         return (
@@ -119,6 +145,7 @@ render() {
                     </div>
                     <div className="uk-width-1-2@s"> 
                         <select name="typeE" onChange={this.changeHandler} value={typeE} required className="uk-select">
+                            <option>----» Selectionner le type de votre Entreprise «----</option>
                             <option value="Entreprise individuelle (EI)">Entreprise individuelle (EI)</option>
                             <option value="Entreprise individuelle à responsabilité limitée (EIRL)">Entreprise individuelle à responsabilité limitée (EIRL)</option>
                             <option value="Entreprise unipersonnelle à responsabilité limitée (EURL)">Entreprise unipersonnelle à responsabilité limitée (EURL)</option>
@@ -149,11 +176,14 @@ render() {
                         </div>
                         <div className="uk-grid-small" data-uk-grid>
                             <div className="uk-width-2-3">
-                                <input disabled name='listAssoc' value={listAssocies} onChange={this.changeHandler} className="uk-input" type="text" id="listAssoc" />
-                                <input disabled name='nbrAssocies' id="nbrAssocies" value={nbrAssocies} onChange={this.changeHandler} hidden type="number" />
+                                <input  disabled placeholder="Liste Associés" name='listAssocies' value={listAssocies} onChange={this.changeHandlerA} className="uk-input" type="text" id="listAssocies" />
+                                <input name='nbrAssocies'  value={nbrAssocies} onChange={this.changeHandler} hidden type="number" id="nbrAssocies" />
                             </div>
                             <div className="uk-width-1-3">
                                 <button type="button" className="uk-button uk-button-secondary" onClick={minA}>-</button>
+                            </div>
+                            <div className="uk-width-1-1">
+                                <button type="button" className="uk-button uk-button-primary" onClick={this.changeHandlerA}><span data-uk-icon="icon: check;"></span> Valider la liste</button>
                             </div>
                         </div>
                     </div>
@@ -169,10 +199,13 @@ render() {
                         </div>
                         <div className="uk-grid-small" data-uk-grid>
                             <div className="uk-width-2-3">
-                                <input disabled name='listGerant' value={listGerant} onChange={this.changeHandler} className="uk-input" type="text" id="liste" />
+                                <input disabled placeholder="Liste Gérants" name='listGerant' value={listGerant} onChange={this.changeHandler} className="uk-input" type="text" id="liste" />
                             </div>
                             <div className="uk-width-1-3">
                                 <button type="button" className="uk-button uk-button-secondary" onClick={min}>-</button>
+                            </div>
+                            <div className="uk-width-1-1">
+                                <button type="button" className="uk-button uk-button-primary" onClick={this.changeHandlerG}><span data-uk-icon="icon: check;"></span> Valider la liste</button>
                             </div>
                         </div>
                     </div>
